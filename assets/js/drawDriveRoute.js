0fdba201b3b6c00d4b6423d6a5ace88a098833a2
Alan Butler <rabutler@usbr.gov>
2016-01-22 09:02:36 -0600

    modified drawing function to accept object and path variables

--- assets/js/drawDriveRoute.js
+++ assets/js/drawDriveRoute.js
@@ -1,29 +1,29 @@
 // adapted from: 
 // https://jakearchibald.com/2013/animated-line-drawing-svg/
 
-var draw = function(){
-var svgObj, svgDoc;
+var draw = function(mySvgObj, myPath){
+	var svgObj, svgDoc;
 
-svgObj = document.getElementById('driveRouteObj');
-svgDoc = svgObj.contentDocument;
+	svgObj = document.getElementById(mySvgObj);
+	svgDoc = svgObj.contentDocument;
 
-var path = svgDoc.getElementById('drivePath');
-var length = path.getTotalLength();
-// Clear any previous transition
-path.style.transition = path.style.WebkitTransition = 'none';
-// Set up the starting positions
-path.style.strokeDasharray = length + ' ' + length;
-path.style.strokeDashoffset = length;
-// Trigger a layout so styles are calculated & the browser
-// picks up the starting position before animating
-path.getBoundingClientRect();
-// Define our transition
-path.style.transition = path.style.WebkitTransition =
-  'stroke-dashoffset 10s linear';
-// Go!
-path.style.strokeDashoffset = '0';
+	var path = svgDoc.getElementById(myPath);
+	var length = path.getTotalLength();
+	// Clear any previous transition
+	path.style.transition = path.style.WebkitTransition = 'none';
+	// Set up the starting positions
+	path.style.strokeDasharray = length + ' ' + length;
+	path.style.strokeDashoffset = length;
+	// Trigger a layout so styles are calculated & the browser
+	// picks up the starting position before animating
+	path.getBoundingClientRect();
+	// Define our transition
+	path.style.transition = path.style.WebkitTransition =
+	  'stroke-dashoffset 10s linear';
+	// Go!
+	path.style.strokeDashoffset = '0';
 }
 	
 window.onload = function(){	
-	draw();
+	draw('driveRouteObj', 'drivePath');
 }
